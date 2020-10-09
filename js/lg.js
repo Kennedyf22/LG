dcbfe9d15db7fb9afc5454f68040d84e5a6964c5
Kennedyf22 <kennedy.fpl22@gmail.com>
2020-10-09 17:18:10 -0300

    Added login

---
+++ js/lg.js
@@ -0,0 +1,11 @@
+function Login(){
+var done=0;
+var password=document.login.password.value;
+password=password.toLowerCase();
+if (password=="220298") { window.location="https://kennedyf22.github.io/LG/LG.html"; done=1; }
+if (password=="2020062") { window.location="https://kennedyf22.github.io/LG/LG.html"; done=1; }
+if (password=="2020061") { window.location="https://kennedyf22.github.io/LG/LG.html"; done=1; }
+if (password=="2020051") { window.location="https://kennedyf22.github.io/LG/LG.html"; done=1; }
+if (password=="2020033") { window.location="https://kennedyf22.github.io/LG/LG.html"; done=1; }
+if (done==0) { alert("Senha ou Usuário inválido."); }
+}
