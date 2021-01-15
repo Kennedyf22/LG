94c5fa566f3c6762ea86b418bae7c542b7a647f9
Kennedyf22 <kennedy.fpl22@gmail.com>
2021-01-15 13:41:20 -0300

    Added warstop.html and update lg.js and messages.css

--- js/lg.js
+++ js/lg.js
@@ -2,14 +2,14 @@ function Login(){
 var done=0;
 var password=document.login.password.value;
 password=password.toLowerCase();
-if (password=="220298") { window.location="https://kennedyf22.github.io/LG/LG.html"; done=1; }
-if (password=="2020062") { window.location="https://kennedyf22.github.io/LG/LG.html"; done=1; }
-if (password=="2020061") { window.location="https://kennedyf22.github.io/LG/LG.html"; done=1; }
-if (password=="2020051") { window.location="https://kennedyf22.github.io/LG/LG.html"; done=1; }
-if (password=="2020033") { window.location="https://kennedyf22.github.io/LG/LG.html"; done=1; }
-if (password=="2020101") { window.location="https://kennedyf22.github.io/LG/LG.html"; done=1; }
-if (password=="2020111") { window.location="https://kennedyf22.github.io/LG/warstop.html"; done=1; }
-if (password=="2020121") { window.location="https://kennedyf22.github.io/LG/LG.html"; done=1; }
+if (password=="220298") { window.location="https://kennedyf22.github.io/LG/LG.html"; done=1; }/*K*/
+if (password=="2020062") { window.location="https://kennedyf22.github.io/LG/LG.html"; done=1; }/*RLS*/
+if (password=="2020061") { window.location="https://kennedyf22.github.io/LG/LG.html"; done=1; }/*ZFL*/
+if (password=="2020051") { window.location="https://kennedyf22.github.io/LG/LG.html"; done=1; }/*MOG*/
+if (password=="2020033") { window.location="https://kennedyf22.github.io/LG/LG.html"; done=1; }/*KRS*/
+if (password=="2020101") { window.location="https://kennedyf22.github.io/LG/LG.html"; done=1; }/*JRS*/
+if (password=="2020111") { window.location="https://kennedyf22.github.io/LG/warstop.html"; done=1; }/*ZO*/
+if (password=="2020121") { window.location="https://kennedyf22.github.io/LG/LG.html"; done=1; }/*JVPR*/
 if (done==0) { alert("Senha ou Usuário inválido."); }
 }
 
