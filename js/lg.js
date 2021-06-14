1f1df221ed25e030518066677b8482ee1104c7b1
Kennedy Fabio <58262446+Kennedyf22@users.noreply.github.com>
2021-06-14 19:11:17 -0300

    Upload all the files
    Upload all the files

--- js/lg.js
+++ js/lg.js
@@ -1,32 +1,31 @@
 function Login(){
 var done=0;
 var password=document.login.password.value;
+var matricula=document.login.matricula.value;
 password=password.toLowerCase();
-if (password=="220298") { window.location="https://kennedyf22.github.io/LG/LG.html"; done=1; }/*K*/
-if (password=="2020062") { window.location="https://kennedyf22.github.io/LG/warstop.html"; done=1; }/*RLS*/
-if (password=="2020061") { window.location="https://kennedyf22.github.io/LG/warstop.html"; done=1; }/*ZFL*/
-if (password=="2020051") { window.location="https://kennedyf22.github.io/LG/warstop.html"; done=1; }/*MOG*/
-if (password=="2020033") { window.location="https://kennedyf22.github.io/LG/warstop.html"; done=1; }/*KRS*/
-if (password=="2020101") { window.location="https://kennedyf22.github.io/LG/LG.html"; done=1; }/*JRS*/
-if (password=="2020111") { window.location="https://kennedyf22.github.io/LG/warstop.html"; done=1; }/*ZO*/
-if (password=="2020121") { window.location="https://kennedyf22.github.io/LG/warstop.html"; done=1; }/*JVPR*/
-if (password=="2021031") { window.location="https://kennedyf22.github.io/LG/warstop.html"; done=1; }/*LGB*/
-if (password=="2021032") { window.location="https://kennedyf22.github.io/LG/LG.html"; done=1; }/*ISA*/
-if (password=="2021033") { window.location="https://kennedyf22.github.io/LG/warstop.html"; done=1; }/*APG*/
-if (password=="2021034") { window.location="https://kennedyf22.github.io/LG/LG.html"; done=1; }/*GRS*/
+matricula=matricula.toLowerCase();
+if (password=="admin"&matricula=="220298") { window.location="https://kennedyf22.github.io/LG/LG.html"; done=1; }/*K*/
+if (password=="04102004"&matricula=="2020062") { window.location="https://kennedyf22.github.io/LG/warstop.html"; done=1; }/*RLS*/
+if (password=="05021978"&matricula=="2020061") { window.location="https://kennedyf22.github.io/LG/warstop.html"; done=1; }/*ZFL*/
+if (password=="19041996"&matricula=="2020051") { window.location="https://kennedyf22.github.io/LG/warstop.html"; done=1; }/*MOG*/
+if (password=="25072002"&matricula=="2020033") { window.location="https://kennedyf22.github.io/LG/warstop.html"; done=1; }/*KRS*/
+if (password=="15111978"&matricula=="2020101") { window.location="https://kennedyf22.github.io/LG/LG.html"; done=1; }/*JRS*/
+if (password=="15111975"&matricula=="2020111") { window.location="https://kennedyf22.github.io/LG/warstop.html"; done=1; }/*ZO*/
+if (password=="04122003"&matricula=="2020121") { window.location="https://kennedyf22.github.io/LG/warstop.html"; done=1; }/*JVPR*/
+if (password=="12031991"&matricula=="2021031") { window.location="https://kennedyf22.github.io/LG/warstop.html"; done=1; }/*LGB*/
+if (password=="28032006"&matricula=="2021032") { window.location="https://kennedyf22.github.io/LG/LG.html"; done=1; }/*ISA*/
+if (password=="30121985"&matricula=="2021033") { window.location="https://kennedyf22.github.io/LG/warstop.html"; done=1; }/*APG*/
+if (password=="19042001"&matricula=="2021034") { window.location="https://kennedyf22.github.io/LG/LG.html"; done=1; }/*GRS*/
 if (done==0) { alert("Senha ou Usuário inválido."); }
 }
 
+/*Tempo de inatividade*/
 function noBack(){window.history.forward()
 }
-
 noBack();
-
 window.onload=noBack;
-
 window.onpageshow=function(evt){if(evt.persisted)noBack()
 }
-
 window.onunload=function(){void(0)
 }
 setTimeout(function() {
