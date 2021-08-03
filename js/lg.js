ca30c1488df4fd150bbdfbfa6e06947e43411dba
Kennedy Fabio <58262446+Kennedyf22@users.noreply.github.com>
2021-08-03 12:51:43 -0300

    Upload new informations
    Upload new informations

--- js/lg.js
+++ js/lg.js
@@ -9,13 +9,13 @@ if (password=="04102004"&matricula=="2020062") { window.location="https://kenned
 if (password=="05021978"&matricula=="2020061") { window.location="https://kennedyf22.github.io/LG/warstop.html"; done=1; }/*ZFL*/
 if (password=="19041996"&matricula=="2020051") { window.location="https://kennedyf22.github.io/LG/warstop.html"; done=1; }/*MOG*/
 if (password=="25072002"&matricula=="2020033") { window.location="https://kennedyf22.github.io/LG/warstop.html"; done=1; }/*KRS*/
-if (password=="15111978"&matricula=="2020101") { window.location="https://kennedyf22.github.io/LG/LG.html"; done=1; }/*JRS*/
+if (password=="15111978"&matricula=="2020101") { window.location="https://kennedyf22.github.io/LG/warstop.html"; done=1; }/*JRS*/
 if (password=="15111975"&matricula=="2020111") { window.location="https://kennedyf22.github.io/LG/warstop.html"; done=1; }/*ZO*/
 if (password=="04122003"&matricula=="2020121") { window.location="https://kennedyf22.github.io/LG/warstop.html"; done=1; }/*JVPR*/
 if (password=="12031991"&matricula=="2021031") { window.location="https://kennedyf22.github.io/LG/warstop.html"; done=1; }/*LGB*/
-if (password=="28032006"&matricula=="2021032") { window.location="https://kennedyf22.github.io/LG/LG.html"; done=1; }/*ISA*/
+if (password=="28032006"&matricula=="2021032") { window.location="https://kennedyf22.github.io/LG/warstop.html"; done=1; }/*ISA*/
 if (password=="30121985"&matricula=="2021033") { window.location="https://kennedyf22.github.io/LG/warstop.html"; done=1; }/*APG*/
-if (password=="19042001"&matricula=="2021034") { window.location="https://kennedyf22.github.io/LG/LG.html"; done=1; }/*GRS*/
+if (password=="19042001"&matricula=="2021034") { window.location="https://kennedyf22.github.io/LG/warstop.html"; done=1; }/*GRS*/
 if (done==0) { alert("Senha ou Usuário inválido."); }
 }
 
